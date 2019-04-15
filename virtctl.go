47cf57ac6bde7174b327a3c26dba1e2eb4dc2c42
syswu <wuheng@iscas.ac.cn>
2019-04-15 14:43:20 +0800

    [Bug]: unable to find object

--- virtctl.go
+++ virtctl.go
@@ -17,84 +17,70 @@ import (
  */
 
  func main() {
-
-
- 	//domcfg := libvirtxml.Domain{}
- 	fmt.Println(DoAnalyse(reflect.TypeOf(libvirtxml.DomainVideo{})))
- 	//fmt.Println(DoAnalyse(reflect.TypeOf(domcfg)))
-
+ 	fmt.Println(DoAnalyse(reflect.TypeOf(libvirtxml.Domain{})))
  }
 
-func DoAnalyse(objType reflect.Type) (string) {
+ func DoAnalyse(objType reflect.Type) (string) {
+ 	return analyse(objType, "<os>", "<os>", "</os>")
+ }
 
-	xmlinfo  := ""
-	endelem  := ""
-	previous := ""
+ func analyse(objType reflect.Type, xmlinfo string, previous string, endelem string) (string) {
 
 	for i := 0; i < objType.NumField(); i++ {
+
 		field := objType.Field(i)
-		//fmt.Printf("%s: %v = %v: %s\n", field.Name, field.Type, field.Tag)
 		xmlTag := field.Tag.Get("xml")
-		//if strings.Contains(xmlTag, "omitempty") ||
-		if len(xmlTag) == 0  || strings.EqualFold(xmlTag, "-"){
+
+		if len(endelem) == 0 {
+			attr := strings.Split(xmlTag, ",")[0]
+			endelem  = "</" + attr  + ">"
+			previous = "<" + attr + ">"
+			xmlinfo  = "<" + attr + ">"
+		} else if len(xmlTag) == 0 || strings.EqualFold(xmlTag, "-") || len(strings.Split(xmlTag, ",")[0]) == 0 {
 			continue
 		} else if strings.Contains(xmlTag, "attr") {
-			if previous == "" {
-				//fmt.Println(str + "%s=%s", objType, xmlTag)
-				continue
+			old := previous
+			new := ""
+			if strings.EqualFold(previous[1:len(previous) - 1], strings.Split(xmlTag, ",")[0])        {
+				new =  "<" +
+							strings.Split(xmlTag, ",")[0] +
+							"=\"" + field.Type.String() + "\">"
+			} else {
+				new =  previous[0:len(previous)-1] + " " +
+							strings.Split(xmlTag, ",")[0] +
+							"=\"" + field.Type.String() + "\">"
 			}
-
-			old :=  previous
-			new :=  previous[0:len(previous)-1] + " " +
-						strings.Split(xmlTag, ",")[0] +
-						"=\"" + field.Type.String() + "\">"
 			xmlinfo = strings.Replace(xmlinfo, old, new, -1)
 			previous = new
 		} else {
+			if strings.Contains(field.Type.String(), "libvirtxml") {
+				attr := strings.Split(xmlTag, ",")[0]
+				end  := "</" + attr  + ">"
+				prev := "<" + attr + ">"
+				name := field.Type.String()
+				if strings.Contains(name, "*") {
+					previous = analyse(reflect.TypeOf(reflect.New(field.Type.Elem()).Elem().Interface()), prev, prev, end)
+				} else if strings.Contains(name, "[]") {
+					previous = analyse(reflect.New(field.Type.Elem()).Elem().Type(), prev, prev, end)
+				} else {
+					previous = analyse(reflect.TypeOf(reflect.New(field.Type).Elem().Interface()), prev, prev, end)
+				}
+			} else {
+				previous = "<" + strings.Split(xmlTag, ",")[0] + ">" +
+					field.Type.String() +
+					"</" + strings.Split(xmlTag, ",")[0] + ">"
+
+			}
 
 			if !strings.Contains(xmlinfo, previous) {
 				xmlinfo += previous
 			}
 
-			if len(endelem) == 0 {
-				if !strings.Contains(xmlTag, ",") {
-					endelem  = "</" + xmlTag + ">"
-					previous = "<" + xmlTag + ">"
-					xmlinfo  = "<" + xmlTag + ">"
-				} else {
-					xmlTag = xmlTag[1: ]
-					endelem  = "</" + xmlTag + ">"
-					previous = "<" + xmlTag + ">"
-					xmlinfo  = "<" + xmlTag + ">"
-				}
-			} else {
-
-				if strings.Contains(field.Type.String(), "libvirtxml") {
-					name := field.Type.String()
-					if strings.Contains(name, "*") {
-						previous = DoAnalyse(reflect.TypeOf(reflect.New(field.Type.Elem()).Elem().Interface()))
-					} else if strings.Contains(name, "[]") {
-						fmt.Println(reflect.New(field.Type.Elem()).Elem().Type())
-						previous = DoAnalyse(reflect.New(field.Type.Elem()).Elem().Type())
-					} else {
-						previous = DoAnalyse(reflect.TypeOf(reflect.New(field.Type).Elem().Interface()))
-					}
-				} else {
-					previous = "<" + xmlTag + ">" +
-						field.Type.String() +
-						"</" + xmlTag + ">"
-				}
-
-			}
-
 		}
 	}
 
-	if len(xmlinfo) == 0 {
-		return previous + endelem
-	} else {
-		return xmlinfo + endelem
-	}
+	return xmlinfo + endelem
+
 }
 
 
