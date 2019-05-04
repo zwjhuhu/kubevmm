d7eed796b55527e68ae12b4798bc36cef1b191d3
syswu <wuheng@iscas.ac.cn>
2019-05-04 10:44:01 +0800

    [Fearure] Support json to xml

--- executor/kubevirt/virtctl.py
+++ executor/kubevirt/virtctl.py
@@ -5,32 +5,36 @@ Copyright (2019, ) Institute of Software, Chinese Academy of Sciences
 @author: wuyuewen@otcaix.iscas.ac.cn
 @author: shizhonghao17@otcaix.iscas.ac.cn
 
+https://pypi.org/project/json2xml/
 https://github.com/kubernetes/kubernetes/issues/51046
 '''
 
-from kubernetes import client, config, watch
+from kubernetes import client, config
+from json import loads
+from xmltodict import unparse
 import libvirt
 import socket
-from lib2to3.pgen2.tokenize import group
-from kubernetes.client.models import v1_resource_field_selector
-from curses.ascii import SO
 
 GROUP = 'cloudplus.io'
 VERSION = 'v1alpha3'
 PLURAL = 'virtualmachines'
 
+def jsontoxml(jsonstr):
+    json = jsonstr.replace('_interface', 'interface').replace(
+        '_', '@').replace('text', '#text').replace('\'', '"')
+    return unparse(loads(json))
+    
+
 def watchkube():
     config.load_kube_config(config_file='/root/.kube/config')
-#     ret = client.CustomObjectsApi().list_cluster_custom_object_with_http_info(
-#         group=GROUP, version=VERSION, plural=PLURAL)
-    v1 = client.CustomObjectsApi()
-    w  = watch.Watch()
-    for event in w.stream(v1.list_cluster_custom_object, group=GROUP, version=VERSION, plural=PLURAL):
-        if event["object"]["spec"]["nodeName"] == socket.gethostname():
-            vmname    = event["object"]["metadata"]["name"]
-            namespace = event["object"]["metadata"]["namespace"]
-            if event["type"] == "ADDED":
-                client.CustomObjectsApi().list_cluster_custom_object(group=GROUP, version=VERSION, plural=PLURAL)
+    thisLabel = "host==" + socket.gethostname()
+    ret = client.CustomObjectsApi().list_cluster_custom_object_with_http_info(
+        group=GROUP, version=VERSION, plural=PLURAL, label_selector=thisLabel)
+
+    for item in ret[0]["items"]:
+        del item["spec"]["nodeName"]
+        print(jsontoxml(str(item['spec'])))
+#   
 
 
 def startVM():
