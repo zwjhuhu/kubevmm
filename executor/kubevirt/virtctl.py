d03956ac4320d531eee7add55d1883f150572e36
syswu <wuheng@iscas.ac.cn>
2019-05-08 17:24:37 +0800

    [Update] list kube-apiserver

--- executor/kubevirt/virtctl.py
+++ executor/kubevirt/virtctl.py
@@ -11,6 +11,7 @@ https://github.com/kubernetes/kubernetes/issues/51046
 
 from kubernetes import client, config
 from json import loads
+from json import dumps
 from xmltodict import unparse
 import libvirt
 import socket
@@ -19,23 +20,34 @@ GROUP = 'cloudplus.io'
 VERSION = 'v1alpha3'
 PLURAL = 'virtualmachines'
 
-def jsontoxml(jsonstr):
-    json = jsonstr.replace('_interface', 'interface').replace(
-        '_', '@').replace('text', '#text').replace('\'', '"')
-    return unparse(loads(json))
-    
+VMs = []
 
-def watchkube():
-    config.load_kube_config(config_file='/root/.kube/config')
-    thisLabel = "host==" + socket.gethostname()
+def listVM():
+#     config.load_kube_config(config_file='/root/.kube/config')
+#     thisLabel = "host==" + socket.gethostname()
+    config.load_kube_config(config_file='/home/config')
+    thisLabel = "host==node30"
     ret = client.CustomObjectsApi().list_cluster_custom_object_with_http_info(
         group=GROUP, version=VERSION, plural=PLURAL, label_selector=thisLabel)
 
     for item in ret[0]["items"]:
-        del item["spec"]["nodeName"]
-        print(jsontoxml(str(item['spec'])))
-#   
+#         name = item['metadata']['name']
+       domain = item['spec']
+       del domain['nodeName']
+       image = domain['image']
+       del domain['image']
+       print(jsontoxml(str(domain).replace('None', "''"))) 
 
+def jsontoxml(jsonstr):
+    json = jsonstr.replace('_interface', 'interface').replace('_transient', 'transient').replace(
+        'suspend_to_mem', 'suspend-to-mem').replace('suspend_to_disk', 'suspend-to-disk').replace(
+            'on_crash', 'on-crash').replace('on_poweroff', 'on-poweroff').replace('on_reboot', 'on-reboot').replace(
+            'nested_hv', "nested-hv").replace('_', '@').replace('text', '#text').replace('\'', '"')
+    return unparse(loads(json))
+    
+'''
+   VM lifecycle
+'''
 
 def startVM():
     conn = libvirt.open("qemu:///system")
@@ -45,6 +57,6 @@ def startVM():
         conn.createLinux(xml)
 
 if __name__ == '__main__':
-     watchkube()
+     listVM()
 
     
