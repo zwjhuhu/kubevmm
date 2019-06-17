5f24143427f1629e83b7122a3dea52c1e161dddc
Frank Wu <suhupt_f@126.com>
2019-06-17 17:55:54 +0800

    API: create kvm virtual machine

--- executor/kubevirt/virtctl.py
+++ executor/kubevirt/virtctl.py
@@ -13,8 +13,10 @@ from kubernetes import client, config
 from json import loads
 from json import dumps
 from xmltodict import unparse
+import os
+import subprocess
 import libvirt
-import socket
+import socket, random
 
 GROUP = 'cloudplus.io'
 VERSION = 'v1alpha3'
@@ -26,17 +28,19 @@ def listVM():
 #     config.load_kube_config(config_file='/root/.kube/config')
 #     thisLabel = "host==" + socket.gethostname()
     config.load_kube_config(config_file='/home/config')
-    thisLabel = "host==node30"
+    thisLabel = "host==node22"
     ret = client.CustomObjectsApi().list_cluster_custom_object_with_http_info(
         group=GROUP, version=VERSION, plural=PLURAL, label_selector=thisLabel)
+    
+    print ret
 
-    for item in ret[0]["items"]:
-#         name = item['metadata']['name']
-       domain = item['spec']
-       del domain['nodeName']
-       image = domain['image']
-       del domain['image']
-       print(jsontoxml(str(domain).replace('None', "''"))) 
+#     for item in ret[0]["items"]:
+# #         name = item['metadata']['name']
+#         domain = item['spec']
+#         del domain['nodeName']
+#         image = domain['image']
+#         del domain['image']
+#         print(jsontoxml(str(domain).replace('None', "''"))) 
 
 def jsontoxml(jsonstr):
     json = jsonstr.replace('_interface', 'interface').replace('_transient', 'transient').replace(
@@ -55,8 +59,45 @@ def startVM():
     for domain in domList:
         xml = domain.XMLDesc()
         conn.createLinux(xml)
+        
+def randomUUID():
+    u = [random.randint(0, 255) for ignore in range(0, 16)]
+    u[6] = (u[6] & 0x0F) | (4 << 4)
+    u[8] = (u[8] & 0x3F) | (2 << 6)
+    return "-".join(["%02x" * 4, "%02x" * 2, "%02x" * 2, "%02x" * 2,
+                     "%02x" * 6]) % tuple(u)
+
+def randomMAC():
+    mac = [ 0x52, 0x54, 0x00,
+        random.randint(0x00, 0x7f),
+        random.randint(0x00, 0xff),
+        random.randint(0x00, 0xff) ]
+    return ':'.join(map(lambda x: "%02x" % x, mac))
+        
+def createVM(options):
+    vm_uuid = randomUUID() if not options.get("uuid") else options.get("uuid")
+    vm_mac = randomMAC() if not options.get("mac") else options.get("mac")
+    vm_name = options.get("name", "vm-" + vm_uuid[0:8])
+    vm_cpus = options.get("cpu", 1)
+    vm_mem_mb = options.get("mem-mb", 1024)
+    vm_cdrom = options.get("cd-rom", "/root/ISO/CentOS-7-x86_64-Minimal-1511.iso")
+    vm_disk_size_gb = options.get("disk-size-gb", 10)
+    vm_net_bridge = options.get("net-bridge", "virbr0")
+    create_vm_command = "virt-install --name %s --virt-type kvm " \
+    "--vcpus %s --ram %s --cdrom=%s " \
+    "--disk size=%s,format=qcow2  --network bridge=%s,mac=%s " \
+    "--graphics vnc,listen=0.0.0.0 --noautoconsole" \
+    % (vm_name, str(vm_cpus), str(vm_mem_mb), vm_cdrom, str(vm_disk_size_gb), vm_net_bridge, vm_mac)
+#     print create_vm_command
+    p = subprocess.Popen(create_vm_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.stdout.readlines()
+    p.stderr.readlines()
+    p.stdout.close()
+    p.stderr.close()
 
 if __name__ == '__main__':
-     listVM()
+    listVM()
+    options = {}
+    createVM(options)
 
     
